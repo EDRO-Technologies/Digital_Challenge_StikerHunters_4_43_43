45da5a1227f70cdb315467eceae8dbd738bdc5c0
ilyas <ilya.surgutskij11@mail.ru>
2024-11-16 23:25:47 +0500

    bug fix

--- IT_platform/Events/models.py
+++ IT_platform/Events/models.py
@@ -3,24 +3,15 @@ from django.db.migrations import CreateModel
 from django.db.models import Model, CASCADE
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    username = models.OneToOneField(User, max_length=100, primary_key=True, on_delete=CASCADE)
-    name = models.CharField(max_length=80)
-    second_name = models.CharField(max_length=80)
-    surname = models.CharField(max_length=80)
-    birth = models.DateField(auto_now_add=True)
-    is_organizer = models.BooleanField(default=False, help_text="Я организатор")
-    category = models.CharField(max_length=100)
-    special = models.CharField(max_length=100)
-    github = models.URLField()
-    portfolio = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
-    image = models.ImageField(upload_to='image/event/', null=True)
-
 class Organizer(models.Model):
     title = models.CharField(max_length=80, primary_key=True)
     contact = models.TextField()
     description = models.TextField()
     image = models.ImageField(upload_to="image/organizator", null = True)
+
+    def __str__(self):
+        return self.title
+
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
@@ -35,3 +26,21 @@ class Event(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Profile(models.Model):
+    username = models.OneToOneField(User, max_length=100, primary_key=True, on_delete=CASCADE)
+    name = models.CharField(max_length=80)
+    second_name = models.CharField(max_length=80)
+    surname = models.CharField(max_length=80)
+    birth = models.DateField(auto_now_add=True)
+    is_organizer = models.BooleanField(default=False, help_text="Я организатор")
+    category = models.CharField(max_length=100)
+    special = models.CharField(max_length=100)
+    github = models.URLField()
+    portfolio = models.ForeignKey(Event, on_delete=models.DO_NOTHING)
+    image = models.ImageField(upload_to='image/event/', null=True)
+
+    def __str__(self):
+        return self.username
+
